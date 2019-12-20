5668246c58277ecc60642fdae25a3c75afd5c374
Joshua Sanders <ninjosh2@gmail.com>
2019-12-20 01:23:57 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,31 +3,18 @@ require 'pry'
 class String
 
   def sentence?
-    if self.end_with?(".")
-      return true
-    else
-      return false
-    end
+    "HI!".end_with?("!")
   end
 
   def question?
-    if self.end_with?("?")
-      return true
-    else
-      return false
-    end
+
   end
 
   def exclamation?
-    if self.end_with?("!")
-      return true
-    else
-      return false
-    end
+
   end
 
   def count_sentences
-    new_array = self.split(/[.?!]/).reject {|string| string.empty?}
-    return new_array.length
+
   end
 end
