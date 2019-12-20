e01ef43a4292148730b7df9f9ba2411b4223fe5c
Joshua Sanders <ninjosh2@gmail.com>
2019-12-20 04:38:51 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -7,11 +7,7 @@ class String
   end
 
   def question?
-    if self.end_with?("?") 
-      return true 
-    else 
-      return false 
-    end 
+    self.end_with?("?") 
   end
 
   def exclamation?
